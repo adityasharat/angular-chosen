9fd35aec54e2f2aecd293741c8062ca3ce8cd6be
Aditya Sharat <adityasharat@gmail.com>
2014-08-03 16:15:11 +0530

    update gh-pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+/*global angular, $*/
+(function (angular, $) {
+    var AngularChosenDemo = angular.module('angular.chosen.demo', ['ngResource', 'angular.chosen']);
+
+    AngularChosenDemo.factory('Countries', ['$resource',
+        function ($resource) {
+            return $resource('data/countries.json');
+        }
+    ])
+
+    AngularChosenDemo.controller('demoController', ['$scope', 'Countries',
+        function ($scope, Countries) {
+            $scope.countries = Countries.query();
+        }
+    ]);
+
+}(angular, $));
