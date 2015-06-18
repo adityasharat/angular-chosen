638ced0c212c62c8aa1def13f3003126d8eb681d
Aditya Sharat <aditya.sharat@flipkart.com>
2015-06-18 17:49:15 +0530

    updates

--- javascripts/angular-chosen.js
+++ javascripts/angular-chosen.js
@@ -4,105 +4,110 @@
  * http://adityasharat.github.io/angular-chosen/
  */
 (function (angular) {
-    var AngularChosen = angular.module('angular.chosen', []);
+  var AngularChosen = angular.module('angular.chosen', []);
 
-    AngularChosen.directive('chosen', function () {
-        var EVENTS, scope, linker, watchCollection;
 
-        /*
-         * List of events and the alias used for binding with angularJS
-         */
-        EVENTS = [{
-            onChange: 'change'
-        }, {
-            onReady: 'chosen:ready'
-        }, {
-            onMaxSelected: 'chosen:maxselected'
-        }, {
-            onShowDropdown: 'chosen:showing_dropdown'
-        }, {
-            onHideDropdown: 'chosen:hiding_dropdown'
-        }, {
-            onNoResult: 'chosen:no_results'
-        }];
+  function chosen($timeout) {
+    var EVENTS, scope, linker, watchCollection;
 
-        /*
-         * Items to be added in the scope of the directive
-         */
-        scope = {
-            options: '=', // the options array
-            ngModel: '=', // the model to bind to,,
-            ngDisabled: '=',
-        };
+    /*
+     * List of events and the alias used for binding with angularJS
+     */
+    EVENTS = [{
+      onChange: 'change'
+    }, {
+      onReady: 'chosen:ready'
+    }, {
+      onMaxSelected: 'chosen:maxselected'
+    }, {
+      onShowDropdown: 'chosen:showing_dropdown'
+    }, {
+      onHideDropdown: 'chosen:hiding_dropdown'
+    }, {
+      onNoResult: 'chosen:no_results'
+    }];
 
-        /*
-         * initialize the list of items
-         * to watch to trigger the chosen:updated event
-         */
-        watchCollection = [];
-        Object.keys(scope).forEach(function (scopeName) {
-            watchCollection.push(scopeName);
-        });
+    /*
+     * Items to be added in the scope of the directive
+     */
+    scope = {
+      options: '=', // the options array
+      ngModel: '=', // the model to bind to,,
+      ngDisabled: '='
+    };
 
-        /*
-         * Add the list of event handler of the chosen
-         * in the scope.
-         */
-        EVENTS.forEach(function (event) {
-            var eventNameAlias = Object.keys(event)[0];
-            scope[eventNameAlias] = '=';
-        });
-
-        /* Linker for the directive */
-        linker = function ($scope, iElm, iAttr) {
-            var maxSelection = parseInt(iAttr.maxSelection, 10),
-                searchThreshold = parseInt(iAttr.searchThreshold, 10);
-
-            if (isNaN(maxSelection) || maxSelection === Infinity) {
-                maxSelection = undefined;
-            }
-
-            if (isNaN(searchThreshold) || searchThreshold === Infinity) {
-                searchThreshold = undefined;
-            }
-
-            iElm.chosen({
-                width: '100%',
-                max_selected_options: maxSelection,
-                disable_search_threshold: searchThreshold,
-                search_contains: true
-            });
-
-            iElm.on('change', function () {
-                iElm.trigger('chosen:updated');
-            });
-
-            $scope.$watchGroup(watchCollection, function () {
-                setTimeout(function () {
-                    iElm.trigger('chosen:updated');
-                }, 100);
-            });
-
-            // assign event handlers
-            EVENTS.forEach(function (event) {
-                var eventNameAlias = Object.keys(event)[0];
-
-                if (typeof $scope[eventNameAlias] === 'function') { // check if the handler is a function
-                    iElm.on(event[eventNameAlias], function (event) {
-                        $scope.$apply(function () {
-                            $scope[eventNameAlias](event);
-                        });
-                    }); // listen to the event triggered by chosen
-                }
-            });
-        };
-
-        // return the directive
-        return {
-            name: 'chosen',
-            scope: scope,
-            restrict: 'A',
-            link: linker
-        };
+    /*
+     * initialize the list of items
+     * to watch to trigger the chosen:updated event
+     */
+    watchCollection = [];
+    Object.keys(scope).forEach(function (scopeName) {
+      watchCollection.push(scopeName);
     });
+
+    /*
+     * Add the list of event handler of the chosen
+     * in the scope.
+     */
+    EVENTS.forEach(function (event) {
+      var eventNameAlias = Object.keys(event)[0];
+      scope[eventNameAlias] = '=';
+    });
+
+    /* Linker for the directive */
+    linker = function ($scope, iElm, iAttr) {
+      var maxSelection = parseInt(iAttr.maxSelection, 10),
+        searchThreshold = parseInt(iAttr.searchThreshold, 10);
+
+      if (isNaN(maxSelection) || maxSelection === Infinity) {
+        maxSelection = undefined;
+      }
+
+      if (isNaN(searchThreshold) || searchThreshold === Infinity) {
+        searchThreshold = undefined;
+      }
+
+      var allowSingleDeselect = iElm.attr('allow-single-deselect') !== undefined ? true : false;
+
+      iElm.chosen({
+        width: '100%',
+        max_selected_options: maxSelection,
+        disable_search_threshold: searchThreshold,
+        search_contains: true,
+        allow_single_deselect: allowSingleDeselect
+      });
+
+      iElm.on('change', function () {
+        iElm.trigger('chosen:updated');
+      });
+
+      $scope.$watchGroup(watchCollection, function () {
+        $timeout(function () {
+          iElm.trigger('chosen:updated');
+        }, 100);
+      });
+
+      // assign event handlers
+      EVENTS.forEach(function (event) {
+        var eventNameAlias = Object.keys(event)[0];
+
+        if (typeof $scope[eventNameAlias] === 'function') { // check if the handler is a function
+          iElm.on(event[eventNameAlias], function (event) {
+            $scope.$apply(function () {
+              $scope[eventNameAlias](event);
+            });
+          }); // listen to the event triggered by chosen
+        }
+      });
+    };
+
+    // return the directive
+    return {
+      name: 'chosen',
+      scope: scope,
+      restrict: 'A',
+      link: linker
+    };
+  }
+  AngularChosen.directive('chosen', ['$timeout', chosen]);
 }(angular));
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,38 +1,38 @@
 /*global angular, $*/
 (function (angular, $) {
-    var AngularChosenDemo = angular.module('angular.chosen.demo', ['ngResource', 'angular.chosen']);
+  var AngularChosenDemo = angular.module('angular.chosen.demo', ['ngResource', 'angular.chosen']);
 
-    AngularChosenDemo.factory('Countries', ['$resource',
-        function ($resource) {
-            return $resource('data/countries.json');
-        }
-    ]);
+  AngularChosenDemo.factory('Countries', ['$resource',
+    function ($resource) {
+      return $resource('data/countries.json');
+    }
+  ]);
 
-    AngularChosenDemo.controller('demoController', ['$scope', 'Countries',
-        function ($scope, Countries) {
-            var data = {
-                countries: [],
-                country: [],
-                countriesTest: []
-            };
+  AngularChosenDemo.controller('demoController', ['$scope', 'Countries',
+    function ($scope, Countries) {
+      var data = {
+        countries: [],
+        country: [],
+        countriesTest: []
+      };
 
-            $scope.countries = Countries.query();
-            $scope.countriesMultiple = Countries.query(function (cts) {
-                $scope.countriesTest = [cts[0], cts[1], cts[2]];
-            });
+      $scope.countries = Countries.query();
+      $scope.countriesMultiple = Countries.query(function (cts) {
+        $scope.countriesTest = [cts[0], cts[1], cts[2]];
+      });
 
-            $scope.maxSelected = function () {
-                $scope.maxSelectionDone = true;
-            };
+      $scope.maxSelected = function () {
+        $scope.maxSelectionDone = true;
+      };
 
-            $scope.chosenOpened = function () {
-                $scope.chosenOpenedDone = true;
-            };
+      $scope.chosenOpened = function () {
+        $scope.chosenOpenedDone = true;
+      };
 
-            $scope.chosenClosed = function () {
-                $scope.chosenClosedDone = true;
-            };
-        }
-    ]);
+      $scope.chosenClosed = function () {
+        $scope.chosenClosedDone = true;
+      };
+    }
+  ]);
 
 }(angular, $));
